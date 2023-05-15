614037a37a25985fe01e0d61f3daed2b732fe0ed
Joshua Doust <jalerous@gmail.com>
2023-05-15 20:34:57 +1000

    Improved test coverage

--- BusinessDayCounterTests.cs
+++ BusinessDayCounterTests.cs
@@ -83,23 +83,29 @@ namespace DesignCrowd_Task
             IList<PublicHoliday> testPublicHolidays = new List<PublicHoliday> {
                 new FixedPublicHoliday(12, 25),
                 new WeekdayHoliday(12, 26),
+                new WeekdayHoliday(4, 8),
                 new DayIndexHoliday(1, DayOfWeek.Tuesday, 1),
+                new DayIndexHoliday(5, DayOfWeek.Monday, 2)
             };
 
             IList<DateTime> startDates = new List<DateTime> {
                 new DateTime(2013, 10, 7),
                 new DateTime(2013, 12, 24),
-                new DateTime(2013, 10, 7)
+                new DateTime(2013, 10, 7),
+                new DateTime(2023, 4, 30),
+                new DateTime(2023, 4, 9)
             };
 
             IList<DateTime> endDates = new List<DateTime> {
                 new DateTime(2013, 10, 9),
                 new DateTime(2013, 12, 27),
-                new DateTime(2014, 1, 1)
+                new DateTime(2014, 1, 1),
+                new DateTime(2023, 5, 13),
+                new DateTime(2023, 4, 16)
             };
 
             IList<int> expectedDays = new List<int> {
-                1, 0, 59
+                1, 0, 59, 9, 4
             };
 
             for (var i = 0; i < expectedDays.Count; i++)
